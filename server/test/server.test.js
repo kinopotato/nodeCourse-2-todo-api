dc0ba1592e111fc6bee778b44532b55df9cb63ca
kinopotato <kinoiskino@gmail.com>
2019-05-03 17:54:03 +0800

    added 2 tests, failed but might be due to my laptop

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,9 +4,17 @@ const request = require("supertest");
 const {app} = require("./../server");
 const {Todo} = require("./../models/todo");
 
+const todos = [{
+  text: "01 text todo"
+}, {
+  text: "02 text todo"
+}];
+
 beforeEach((done) => {
-  Todo.deleteMany({}).then(() => done());
-})
+  Todo.deleteMany({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
+});
 
 describe("POST /todos", () => {
   it("should create a new todo", (done) => {
@@ -24,8 +32,8 @@ describe("POST /todos", () => {
           return done(error);
         }
 
-        Todo.find().then((todos) => {
-          expect(todos.length).toBe(1);
+        Todo.find({text}).then((todos) => {
+          expect(todos.length).toBe(3);
           expect(todos[0].text).toBe(text);
           done();
         }).catch((error) => {
@@ -45,11 +53,22 @@ describe("POST /todos", () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch((error) => {
           done(error);
         })
       })
   });
+  })
+
+  describe("GET/todos", () => {
+    it("should get all todos", (done) => {
+      request(app)
+        .get("/todos")
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.todos.length).toBe(2);
+        }).end(done);
+    })
   })
